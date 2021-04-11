5c53effa3328d29acd77327a3a1509e9b4a1f0e1
Jonas Borjesson <jonas@jonasborjesson.com>
2021-04-11 10:07:47 -0700

    Forgot to check in the Format class

---
+++ codec-gtp-base/src/main/java/io/snice/codecs/codec/gtp/v1/common/Format.java
@@ -0,0 +1,8 @@
+package io.snice.codecs.codec.gtp.v1.common;
+
+/**
+ * Only two of types/formats for GTPv1 Information Elements, Type-Value and Type-Length-Value
+ */
+public enum Format {
+    TV, TLV;
+}
